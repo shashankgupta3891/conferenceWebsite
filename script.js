cffef87d83e1b9a589bc705ae0fcca6e22782749
shashank gupta <43991133+shashankgupta3891@users.noreply.github.com>
2020-09-03 14:58:23 +0530

    beautification on code

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
-const nav_items_ul=document.getElementsByClassName('nav_items')[0].children
+// const nav_items_ul=document.getElementsByClassName('nav_items')[0].children
 
 
-console.log(nav_items_ul)
-const pathName=window.location.pathname
+// console.log(nav_items_ul)
+const pathName = window.location.pathname
 
-if(pathName.includes('committee')){
-    document.getElementById("committee").className+='active '
-    
-    
-}
-else if(pathName.includes('topic')){
-    console.log(window.location.pathname)   
-    document.getElementById("topic").className+='active '
+if (pathName.includes('committee')) {
+    document.getElementById("committee").className += 'active '
 
-}else if(pathName.includes('dates')){
-    console.log(window.location.pathname)   
-    document.getElementById("dates").className+='active '
 
-}else if(pathName.includes('call-for-papers')){
-    console.log(window.location.pathname)   
-    document.getElementById("call-for-papers").className+='active '
+} else if (pathName.includes('topic')) {
+    document.getElementById("topic").className += 'active '
 
-}else if(pathName.includes('submission')){
-    console.log(window.location.pathname)   
-    document.getElementById("submission").className+='active '
+} else if (pathName.includes('dates')) {
+    document.getElementById("dates").className += 'active '
 
-}else if(pathName.includes('organization')){
-    console.log(window.location.pathname)   
-    document.getElementById("organization").className+='active '
+} else if (pathName.includes('call-for-papers')) {
+    document.getElementById("call-for-papers").className += 'active '
 
-}else if(pathName.includes('publication')){
-    console.log(window.location.pathname)  
-    document.getElementById("publication").className+='active '
+} else if (pathName.includes('submission')) {
+    document.getElementById("submission").className += 'active '
 
-}else{
-    console.log(window.location.pathname)
-    document.getElementById("home").className+='active '
+} else if (pathName.includes('organization')) {
+    document.getElementById("organization").className += 'active '
+
+} else if (pathName.includes('publication')) {
+    document.getElementById("publication").className += 'active '
+
+} else {
+    document.getElementById("home").className += 'active '
 
 }
 
